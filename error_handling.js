ea57731eb282ee7f896c5f6fa32e14b73930e999
xunga <6796782+xunga@users.noreply.github.com>
2017-09-23 16:24:40 +0200

    added general response message for express

--- error_handling.js
+++ error_handling.js
@@ -2,7 +2,20 @@
  * General purpose error logging
  **/
 function generalCatchCallback(e, functionName) {
-  return console.error('Error executing ' + functionName + ': ', e);
+  return console.error("Error executing " + functionName + ": ", e);
+}
+
+/**
+ * General internal error for express
+ * @param e
+ * @param functionName
+ */
+function expressResponseError(response, message) {
+  let errorMessage = "there was an error in the request";
+  if(message)
+    errorMessage = message
+  response.json({ success: false, error: errorMessage });
 }
 
 module.exports.generalCatchCallback = generalCatchCallback;
+module.exports.expressResponseError = expressResponseError;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('./prototypes/numbers');
 require('./prototypes/objects');
 require('./prototypes/strings');
 
-const { generalCatchCallback } = require('./error_handling');
+const { generalCatchCallback, expressResponseError } = require('./error_handling');
 
 module.exports = generalCatchCallback;
+module.exports = expressResponseError;
